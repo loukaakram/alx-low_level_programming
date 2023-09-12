bacaa7f55cd74f080d2f4e0fcf13145a1f7f788f
Akram <akramloukrimy@gmail.com>
2023-09-12 15:17:16 -0700

    Print alphabet with a void function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return;
+}
