53cff35e6b2a7da4d8b4f9b25757a6a7ca4c773f
AKRAM <akramloukrimy@gmail.com>
2024-02-07 19:28:41 -0800

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/100-main.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+
+    head2 = NULL;
+    add_nodeint_end(&head2, 0);
+    add_nodeint_end(&head2, 1);
+    add_nodeint_end(&head2, 2);
+    add_nodeint_end(&head2, 3);
+    add_nodeint_end(&head2, 4);
+    add_nodeint_end(&head2, 98);
+    add_nodeint_end(&head2, 402);
+    add_nodeint_end(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    head2 = NULL;
+    add_nodeint_end(&head2, 0);
+    add_nodeint_end(&head2, 1);
+    add_nodeint_end(&head2, 2);
+    add_nodeint_end(&head2, 3);
+    add_nodeint_end(&head2, 4);
+    add_nodeint_end(&head2, 98);
+    add_nodeint_end(&head2, 402);
+    add_nodeint_end(&head2, 1024);
+    printf("%p\n", (void *)head2);
+    free_listint_safe(&head2);
+    printf("%p\n", (void *)head2);
+    return (0);
+}
+
